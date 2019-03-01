f2d6100b02dec2f05164a03d4f6a332bcb4fc1e4
WanderingBrooks <cs.jasonbrooks@gmail.com>
2019-03-01 12:19:26 -0500

    Check if play occurs in the text, remove the time if its the only message

--- src/delete-this.js
+++ src/delete-this.js
@@ -1,6 +1,6 @@
 const deleteThis = () => {
   document.querySelectorAll('a[href="#"]').forEach(el => {
-    if ( el.innerText === 'Play' ) {
+    if ( el.innerText.includes('Play') ) {
       const gameMessage = el
         .parentElement
         .parentElement
@@ -16,6 +16,19 @@ const deleteThis = () => {
       if ( gameMessage.parentElement.childElementCount > 2 ) {
         gameMessage.parentElement.removeChild( gameMessage );
       } else {
+        if ( 
+          messageChain.parentElement.previousSibling &&
+          messageChain.parentElement.nextSibling &&
+          messageChain.parentElement.previousSibling.tagName === 'H4' &&
+          messageChain.parentElement.nextSibling.tagName === 'H4'
+          ) {
+
+          messageChain
+          .parentElement
+          .parentNode
+          .removeChild( messageChain.parentElement.previousSibling );
+        }
+
         messageChain.parentElement.removeChild( messageChain );
       }
     }

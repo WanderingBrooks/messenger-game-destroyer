a43074e0083b46fe33187fc30c61b03931dd3349
WanderingBrooks <cs.jasonbrooks@gmail.com>
2019-03-12 16:41:40 -0400

    Remove title updating

--- src/the-toggler.js
+++ src/the-toggler.js
@@ -1,4 +1,4 @@
-chrome.pageAction.onClicked.addListener(tab => {
+chrome.pageAction.onClicked.addListener( tab => {
   chrome.tabs.get( tab.id, () => {
     chrome.tabs.sendMessage( tab.id, { toggle: true } );
   });
@@ -6,9 +6,4 @@ chrome.pageAction.onClicked.addListener(tab => {
 
 chrome.runtime.onMessage.addListener(( { detected }, { tab } ) => {
   chrome.pageAction.show( tab.id );
-
-  chrome.pageAction.setTitle({
-    tabid: tab.id,
-    text:  detected.toString()
-  });
 });

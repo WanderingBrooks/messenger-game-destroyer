f45400c15523ebfe4090836a0a589c16dcab51bd
WanderingBrooks <cs.jasonbrooks@gmail.com>
2019-03-11 21:47:49 -0400

    Fix check for removing time and explicitly check for link test

--- src/delete-this.js
+++ src/delete-this.js
@@ -1,6 +1,6 @@
 const deleteThis = () => {
   document.querySelectorAll('a[href="#"]').forEach(el => {
-    if ( el.innerText.includes('Play') ) {
+    if ( el.innerText === 'Play' || el.innerText === 'Play Now' ) {
       const gameMessage = el
         .parentElement
         .parentElement
@@ -16,13 +16,19 @@ const deleteThis = () => {
       if ( gameMessage.parentElement.childElementCount > 2 ) {
         gameMessage.parentElement.removeChild( gameMessage );
       } else {
-        if ( 
+        // Previous sibiling is exists and is an H4 tag
+        const prevSibiling = (
           messageChain.parentElement.previousSibling &&
-          messageChain.parentElement.nextSibling &&
-          messageChain.parentElement.previousSibling.tagName === 'H4' &&
-          messageChain.parentElement.nextSibling.tagName === 'H4'
-          ) {
+          messageChain.parentElement.previousSibling.tagName === 'H4'
+        );
 
+        // Next sibiling does not exist or is an H4 tag
+        const nextSibiling = (
+          !messageChain.parentElement.nextSibling || 
+          messageChain.parentElement.nextSibling.tagName === 'H4'
+        );
+
+        if ( prevSibiling && nextSibiling ) {
           messageChain
           .parentElement
           .parentNode
